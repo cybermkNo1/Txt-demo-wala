d65cb4fa4749fb04036e24c59c19b61a996c5a2d
cybermkNo1 <ak4213367@gmail.com>
2025-10-20 09:07:42 +0530

    Change credit from '𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎' to '𝙎𝙇𝘼𝙔𝘽𝙀𝙍 𝘽𝙊𝙏𝙎'
    Updated the credit information in the vars.py file.

--- modules/vars.py
+++ modules/vars.py
@@ -8,7 +8,7 @@ API_HASH = environ.get("API_HASH", "4e6e068135b733caf2496850989d401e")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 OWNER = int(environ.get("OWNER", "7962530240"))
-CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
+CREDIT = environ.get("CREDIT", "𝙎𝙇𝘼𝙔𝘽𝙀𝙍 𝘽𝙊𝙏𝙎")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
@@ -18,4 +18,5 @@ AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
 
